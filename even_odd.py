7af8ca77e88a4822308d053efb55ea27f48e3739
dd <dd@weboven.net>
2017-04-07 14:33:05 +0300

    fix function exec

--- even_odd.py
+++ even_odd.py
@@ -1,42 +1,41 @@
 """
     1. именуй файлы нормально (меньше пробелов и верхнего регистра)
+    2. кто тебя надоумил ставить пробел перед скобкой? (print ())
 """
 
 
 from datetime import *
-print ('Начало диапазона: ')
+print('Начало диапазона: ')
 t = True
 while t:
     try:
-        user_one = int (input(': '))
+        user_one = int(input(': '))
     except:
-        print ("Только числа")
+        print("Только числа")
         continue
     break
 
-print ('Конец диапазона: ')
+print('Конец диапазона: ')
 p = True
 while p:
     try:
-        user_two = int (input(': '))
+        user_two = int(input(': '))
     except:
-        print ("Только числа")
+        print("Только числа")
         continue
 
 
     if user_two < user_one:
-        print ('Второе больше первого!')
+        print('Второе больше первого!')
         continue
     else:
         break
 
-time = (datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S"))
+time =(datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S"))
 log = open('loge ' + time + '.txt', 'w')
 for i in range(user_one,user_two + 1):
     if i % 2 == 0:
-        log.write (str (i) + '   :четное \n')
+        log.write(str(i) + '   :четное \n')
     else:
         log.write(str(i) + '   :нечетное \n')
-print ('Файл добавлен')
-
-
+print('Файл добавлен')
